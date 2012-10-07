1509a2791748c57a54a6400d3c153032c80db6b0
Shefali Garg <shefalig@iitk.ac.in>
2012-10-08 01:13:45 +0530

    add registration file

---
+++ portal_register.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="my_style.css"/>
+</head>
+<body>
+<div class="container">
+<div class="head">
+<img src="iitk-logo.png" align="left">
+<h1>Department of computer science and engineering
+</br>IIT Kanpur</h1>
+<h2>Event Management Portal</h2>
+</div>
+<div class="book">
+<form name="input" action="pwd.php" method="post">
+<p><label>Username:</label> 
+<input type="text" name="username" /></p>
+<p><label>Password:</label>
+ <input type="password" name="pwd" /></p>
+<p><label>Retype Password:</label>
+ <input type="password" name="pwd2" /></p>
+<p><label><input type="submit" value="Register" /></label></p>
+</form>
+</div>
+</div>
+</body>
+</html>
