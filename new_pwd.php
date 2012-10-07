0eaae71836117a7836ec345a4d30bafc9f3750bc
Shefali Garg <shefalig@iitk.ac.in>
2012-10-08 01:53:31 +0530

    update password

---
+++ new_pwd.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+<form action="change_pwd.php" method="post">
+Username: <input tupe="text" name="username"><br/>
+CurrentPassword: <input type="password" name="pwd"><br/>
+New password:<input type="password" name="pwd2"><br/>
+<input type="submit" value="change password" />
+</form>
